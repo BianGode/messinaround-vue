4cdbbe6973bb83a10e5913aec1c0ff3fbb9db5fa
BianGode <bian2205@gmail.com>
2023-12-17 23:52:11 +0100

    wanted to try a starfield but messed up

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -12,135 +12,345 @@
 
 <template>
   <div class="starWrapper">
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-    <div :on-mouseenter="enlarge" :on-mouseleave="relavate" class="small star"></div>
-
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div><div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div><div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div><div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div><div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div><div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div><div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div><div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div><div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div><div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div><div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div><div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div><div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div><div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div><div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div><div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div><div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div><div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div><div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div><div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div><div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div><div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div><div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div><div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div><div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div><div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div><div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div><div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div><div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div><div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div><div class="starContainer">
+      <div  class="small star"></div>
+    </div>
+    <div class="starContainer">
+      <div  class="small star"></div>
+    </div>
   </div>
 </template>
 
-<style  scoped>
+<style lang="scss" scoped>
+@keyframes enlargeCircle {
+  0% {
+    width: 5px;
+    height: 5px;
+    background-color: white;
+  }
+  25% {
+    background-color: black;
+    height: 8px;
+    width: 8px;
+    border-radius: 0px;
+  }
+  45% {
+    background-color: deeppink;
+    height: 80%;
+    width: 50px;
+    border-radius: 30px;
+  }
+  65% {
+    border-radius: 60px;
+    height: 80%;
+    width: 50px;
+    background-color: deeppink;
+  }
+  75% {
+    background-color: black;
+    height: 8px;
+    width: 8px;
+    border-radius: 90px;
+  }
+  100% {
+    width: 5px;
+    height: 5px;
+    background-color: white;
+    border-radius: 90px;
+  }
+}
 .starWrapper {
   width: 100%;
   display: grid;
   grid-template-columns: 10% 10% 10% 10% 10% 10% 10% 10% 10% 10%;
+  grid-template-rows: 10% 10% 10% 10% 10% 10% 10% 10% 10% 10%;
   height: 400px;
+  position: absolute;
+}
+.starContainer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  // max-width: 10%;
+}
+.starContainer:hover {
+   .star {
+    animation-name: enlargeCircle;
+    animation-duration: 1s;
+   }
 }
 .star {
   background-color: white;
+  border-radius: 90px;
 }
 .small {
-  width:2px;
-  height: 2px;
-  transition: width 0.5s ease;
-}
-.small:hover {
-  width: 10px;
-  height: 10px;
-  transition: width 0.5s ease;
-  background-color: brown;
-}
-.large {
-  width: 10px;
-  height: 10px;
-  transition: width 0.5s ease;
-  background-color: brown;
+  width:5px;
+  height: 5px;
+  transition: width 0.5s ease, height 0.5s ease;
+
 }
 </style>
